d23da38fa4bd64a183879c1fc0a8b9bae5437dc6
Shalev Kubi <shalevku@gmail.com>
2021-11-05 14:14:26 +0200

    Update DB credentials and heroku settings

--- config/db.config.js
+++ config/db.config.js
@@ -1,25 +1,43 @@
 //    Models
 import Sequelize from 'sequelize'
 
-// // Extract ClearDB credentials from Heroku config vars.
-// let tail = process.env.CLEARDB_DATABASE_URL.slice(8,-1)
-// const database = tail.slice(0, tail.indexOf(':'))
-// tail = tail.slice()
-
+// Extract ClearDB credentials from Heroku config vars.
+const dbUrl = process.env.CLEARDB_DATABASE_URL
+const delimiters = [
+  dbUrl.indexOf('//'),
+  dbUrl.lastIndexOf(':'),
+  dbUrl.indexOf('@'),
+  dbUrl.lastIndexOf('/'),
+  dbUrl.indexOf('?')
+]
+const username = dbUrl.slice(delimiters[0] + 2, delimiters[1])
+const password = dbUrl.slice(delimiters[1] + 1, delimiters[2])
+const host = dbUrl.slice(delimiters[2] + 1, delimiters[3])
+const database = dbUrl.slice(delimiters[3] + 1, delimiters[4])
+console.log('db credentials')
+console.log(username)
+console.log(password)
+console.log(host)
+console.log(database)
 // Define the sequelize connection to Mysql.
-const sequelize = new Sequelize('heroku_c2f491eba27d970', 'b907931e57d0ff', 'f3ce955f', {
-  host: 'us-cdbr-east-04.cleardb.com',
-  port: 3306,
-  dialect: 'mysql',
-  define: { freezeTableName: true },
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-  logging: false
-})
+const sequelize = new Sequelize(
+  database,
+  username,
+  password,
+  {
+    host: host,
+    port: 3306,
+    dialect: 'mysql',
+    define: { freezeTableName: true },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    logging: false
+  }
+)
 
 export { sequelize }
 
